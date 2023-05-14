d2a13f2514b6e1728879ecf59e09124231547fe7
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-05-14 18:38:44 -0300

    navigates by dom manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('hello world');
+(() => {
+  const dom = document.querySelector('#content');
+  const main = document.createElement('div');
+
+  const showDescription = () => {
+    main.innerHTML = '';
+    const title = document.createElement('h1');
+    title.textContent = 'Some restaurant';
+    const description = document.createElement('p');
+    description.textContent = 'Welcome to Some Restaurant! At Some Restaurant, we are passionate about creating memorable dining experiences!'
+    main.appendChild(title);
+    main.appendChild(description);
+  };
+
+  const showContact = () => {
+    main.innerHTML = '';
+    const title = document.createElement('h1');
+    title.textContent = 'Contact';
+    const description = document.createElement('p');
+    description.textContent = 'Talk to us at 223344';
+    main.appendChild(title);
+    main.appendChild(description);
+  };
+
+  const navButtons = () => {
+    const description = document.createElement('button');
+    const contact = document.createElement('button');
+    description.textContent = 'Description';
+    contact.textContent = 'Contact';
+    description.addEventListener('click', showDescription);
+    contact.addEventListener('click', showContact);
+    dom.appendChild(description);
+    dom.appendChild(contact);
+  };
+
+  const init = () => {
+    navButtons();
+    dom.appendChild(main);
+    showDescription();
+  };
+
+  init();
+})();
